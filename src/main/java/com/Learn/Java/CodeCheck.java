3f9bc58a3a7630f93ba5e7b2216d84c88226df5f
mohammed.bhuiayan <mohammed.bhuiayan@USABOS279822L.ad.infosys.com>
2020-05-17 11:38:53 -0700

    Push Code

---
+++ src/main/java/com/Learn/Java/CodeCheck.java
@@ -0,0 +1,38 @@
+package com.Learn.Java;
+
+public class CodeCheck {
+	static int x = 100;
+	int a = 10;
+	int b = 5;
+	int c;
+	
+	void addition(){
+		int y = 25;
+		c= a+b;
+		System.out.println(c);
+	 }
+	 
+	 void subtraction() {
+		c= a-b;
+		System.out.println(c);
+	 }
+	 
+	 void multiplication() {
+			c= a*b;
+			System.out.println(c);
+		}
+	 
+	 public static void main(String[] args) {
+		 
+		 int x = 10;
+		 float y = 20;
+		 
+		 System.out.println(x+y);
+		 
+		 
+		 
+		 
+		 
+	}
+	
+}
---
+++ src/main/java/com/Learn/Java/FirstJavaClass.java
@@ -0,0 +1,33 @@
+package com.Learn.Java;
+
+public class FirstJavaClass {
+	int a = 10;
+	int b = 20;
+	
+	
+	void addition(){
+		int c = (a+b);
+		System.out.println(c);
+	}
+		
+	void substraction() {
+		int c =a-b;
+		System.out.println(c);
+	}
+	
+	void multiplication() {
+		int c = a*b;
+		System.out.println(c);
+	}
+
+	
+	public static void main(String[] args) {
+	
+		//ClassName obj = new ClassName();
+		
+		
+
+
+	}
+	
+}
